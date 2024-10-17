7cc84448ae698cb315a32abf3811b01c174261ee
Vladimir <vladimirsabo@yandex.ru>
2024-10-17 21:40:00 +0800

    поправить контроллер

--- src/main/java/com/example/rabbit/controller/SendNotificationToQueueController.java
+++ src/main/java/com/example/rabbit/controller/SendNotificationToQueueController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by vladimirsabo on 17.10.2024
  */
-@RestController("/send")
+@RestController
+@RequestMapping("/send")
 @RequiredArgsConstructor
 public class SendNotificationToQueueController {
     private final SendMessageService sendMessageService;
